d18ad93d04efd27803be03ace13d086592887d91
joshuatsmith96 <joshuatsmith96@gmail.com>
2025-07-24 23:39:26 -0400

    Added new directory tiles

--- src/core/components/Header.tsx
+++ src/core/components/Header.tsx
@@ -6,8 +6,10 @@ const Header = () => {
         <div>
             <div className="flex flex-row items-center gap-3 p-3 px-10 shadow-md">
                 <FontAwesomeIcon icon={faCode} className="p-2 rounded-md text-white bg-gradient-to-r from-blue-400 to-purple-400"/>
-                <h1 className="font-bold text-lg">Lab</h1>
-                <p className="text-gray-500">Joshua's React Component Library</p>
+                <div className="flex flex-col items-start">
+                    <h1 className="font-bold text-lg">Josh's Component Lab</h1>
+                    <p className="text-gray-500">Joshua's React Component Library</p>
+                </div>
             </div>
         </div>
     )
--- src/core/pages/Directory.tsx
+++ src/core/pages/Directory.tsx
@@ -1,9 +1,14 @@
+import Tile from "../components/Tile"
+
 const Directory = () => {
     return(
         <div className="p-10 flex flex-col items-center">
-            <div className="w-[600px] flex flex-col items-center text-center gap-5">
-                <h1 className="font-bold text-4xl">Component Library</h1>
-                <p className="text-lg">Explore and test Joshua's components in an interactive environment built by Josh! This library is free to use.</p>
+            <div className="w-[600px] flex flex-col items-center text-center gap-5 max-md:w-[350px]">
+                <h1 className="font-bold text-4xl max-md:text-3xl">Component Library</h1>
+                <p className="text-lg font-regular max-md:text-md">Explore and test Joshua's components in an interactive environment built by Josh! This library is free to use.</p>
+            </div>
+            <div>
+                <Tile color="purple"/>
             </div>
         </div>
     )
